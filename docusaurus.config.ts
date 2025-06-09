a34d422d1a45271774d020a2b71d41f3b6b1b42a
Matthew MacRae-Bovell <matthewmacraebovell@outlook.com>
2025-06-08 23:04:45 -0400

    Update github link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -93,7 +93,7 @@ const config: Config = {
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/MathyouMB/student-to-software-engineer',
           position: 'right',
           className: 'header-github-link',
           'aria-label': 'GitHub repository',
