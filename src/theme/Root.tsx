4afea36300e2324fb04b63fdf7b5be3c707e1eff
MathyouMB <matthewmacraebovell@outlook.com>
2025-06-22 02:28:40 -0400

    Add telemetry fix

---
+++ src/theme/Root.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode, useEffect } from 'react';
+import { useLocation } from '@docusaurus/router';
+
+declare global {
+  interface Window {
+    Shynet?: {
+      newPageLoad: () => void;
+    };
+  }
+}
+
+type RootProps = {
+  children: ReactNode;
+};
+
+export default function Root({ children }: RootProps) {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window?.Shynet) {
+      window?.Shynet?.newPageLoad()
+    }
+  }, [location.pathname]);
+
+  return (
+    <>
+      {children}
+    </>
+  );
+}
